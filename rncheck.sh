9af9b5069055b509b2730dedf8f438134d1c95c4
G <w_i_n_d_y_o@hotmail.com>
2019-12-12 17:05:13 +0100

    v2
    sanity checks and webhook for discord

--- rncheck.sh
+++ rncheck.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
-year=`cat /home/user/rn-checker/year.dat`
-seasonnum=`cat /home/user/rn-checker/seasonnum.dat`
+year=`cat /rn-checker/year.dat`
+seasonnum=`cat /rn-checker/seasonnum.dat`
 seasons=(spring summer winter)
 release=${seasons[$seasonnum]}$year
-url=https://releasenotes.docs.salesforce.com/en-us/$release/release-notes/salesforce_release_notes.htm
+url="https://releasenotes.docs.salesforce.com/en-us/$release/release-notes/salesforce_release_notes.htm"
 rncode=`curl -s --head $url | head -n 1 | grep "HTTP/1.* 2\d*"`
 base=`curl -s --head https://releasenotes.docs.salesforce.com/en-us/winter19/release-notes/salesforce_release_notes.htm | head -n 1 | grep "HTTP/1.* 2\d*"`
+currentmonth=`date +%B`
+currentdate=`date`
+webhookurl="yourwebhookurl"
+jsoncontent='{"embeds":[{"title": "Preview Release Notes are out!","color": 29951,"description": "The official SF preview release notes are out. Get to work, @Windyo!","fields": [{"name": "Timestamp","value": "'$currentdate'"},{"name": "Link:","value": "'$url'"}],"footer": {"icon_url": "https://cdn.discordapp.com/embed/avatars/0.png","text": "Aaaah, I love the smell of fresh releases in the morning."}}]}'
 if [[ $rncode == $base ]]; then
-        echo "Release Notes Preview are out at $url!" | mail -s "New Release Note Are Out!" youremail@mail.com
-        if [[ $seasonnum == 1 ]]; then
+        if [[($seasonnum == "2" && ($currentmonth="September" || $currentmonth="August")) || ($seasonnum == "0" && ($currentmonth="January" || $currentmonth="February"))  || ($seasonnum == "1" && ($currentmonth="April" || $currentmonth="May")) ]]; then
+                curl -H "Content-Type: application/json" -X POST -d "$jsoncontent" $webhookurl
                 year=$((year+1))
+                fi
+                if [[ $seasonnum == 2 ]]; then
+                        seasonnum=0
+                else
+                        seasonnum=$((seasonnum+1))
+                fi
+                echo "${year}" > /rn-checker/year.dat
+                echo "${seasonnum}" > /rn-checker/seasonnum.dat
         fi
-        if [[ $seasonnum == 2 ]]; then
-                seasonnum=0
-        else
-                seasonnum=$((seasonnum+1))
-        fi
-        echo "${year}" > /home/user/rn-checker/year.dat
-        echo "${seasonnum}" > /home/user/rn-checker/seasonnum.dat
 fi
